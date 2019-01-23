ccec46f6af601b593efa37dc77c37e99cefe3559
rayner-villalba-coderoad-com <rayner.villalba@coderoad.com>
2019-01-23 15:35:07 -0400

    Creating new Project with Material

---
+++ apps/ekklesia/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'ekklesia',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/ekklesia/'
+};
---
+++ apps/ekklesia/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NxModule } from '@nrwl/nx';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    NxModule.forRoot(),
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
