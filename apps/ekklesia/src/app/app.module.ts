afe8ffd6053b68c751443d0c393ed6b21c9e1c05
rayner-villalba-coderoad-com <rayner.villalba@coderoad.com>
2019-01-23 16:11:26 -0400

    Adding Angular Material

--- apps/ekklesia/src/app/app.module.ts
+++ apps/ekklesia/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from "@ekklesia/material";
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     NxModule.forRoot(),
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
